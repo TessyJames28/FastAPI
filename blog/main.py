616e10ee486c889543e31c46c28035dfc46047ce
Tessyjames <tessyjames28@gmail.com>
2025-01-04 12:41:40 +0100

    Retrieve blog with specific id and enure the use of proper response code

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 from . import models
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from .schemas import Blog
 from .database import engine, SessionLocal
 from sqlalchemy.orm.session import Session
@@ -21,7 +21,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: Blog, db:Session = Depends(get_db)):
     # Create blog based on the schema: using request body
     new_blog = models.Blog(title=request.title, body=request.body) 
@@ -37,3 +37,16 @@ def get_all(db: Session = Depends(get_db)):
     # Returns all blogs
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_by_id(id, response: Response, db: Session = Depends(get_db), ):
+    # Retrieve blog based on given id
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with the id {id} not found"}
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail = f"Blog with the id {id} not found")
+    return blog
