6ece86951edbb7349d23577824f0bfd8d6d25303
Tessyjames <tessyjames28@gmail.com>
2025-01-05 13:54:23 +0100

    Created a new models for the response model

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel
 
 
+# Schema for the blog
 class Blog(BaseModel):
     title:str
     body:str
+
+
+
+# Schema for the formatted display of blogs. This allows the display of desired fields
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+
+    class Config():
+        orm_mode = True
+
+    
