6e292f01af7b2241edb92c436f989a02742e0883
Tessyjames <tessyjames28@gmail.com>
2025-01-06 17:40:46 +0100

    Refactored the db

--- blog/database.py
+++ blog/database.py
@@ -14,3 +14,12 @@ SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
 Base = declarative_base()
 
+
+# The function helps to make the Session pased to the db on create func pydantic
+# Thanks to the use of Depends from fastapi which allows the error-free starting of the server
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
