1e2aeae896e14b22ae82f65def70c1339b90ffd9
Tessyjames <tessyjames28@gmail.com>
2025-01-07 23:36:56 +0100

    Refactored the file

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,9 +1,8 @@
 from ..database import get_db
-from fastapi import APIRouter, Depends, status, HTTPException
-from ..hashing import Hash
+from fastapi import APIRouter, Depends
 from ..schemas import User, ShowUser
 from sqlalchemy.orm.session import Session
-from .. import models
+from ..repository import user
 
 
 router = APIRouter(
@@ -15,20 +14,11 @@ router = APIRouter(
 @router.post('', response_model=ShowUser)
 def create_user(request: User, db:Session = Depends(get_db)):
     # Function that creates new user
-    hashed_pwd = Hash.bcrypt(request.password)
-    new_user = models.User(name = request.name, email = request.email, password = hashed_pwd)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create_user(request, db)
 
 
 
 @router.get('/{id}', response_model=ShowUser)
 def get_user(id, db: Session = Depends(get_db)):
     # Retrieve user based on given id
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
-                            detail = f"User with the id {id} not found")
-    return user
+    return user.get_users(id, db)
