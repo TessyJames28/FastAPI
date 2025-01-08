4dd05ec39e89b77a2a3aec9be7c1a39be6aaf1cb
Tessyjames <tessyjames28@gmail.com>
2025-01-08 22:58:10 +0100

    Added a new router in the main file

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
 from . import models
 from fastapi import FastAPI
-from .routers import blog, user
+from .routers import blog, user, authLogin
 from .database import engine
 
 
@@ -12,5 +12,6 @@ models.Base.metadata.create_all(engine)
 
 
 # Router definition
+app.include_router(authLogin.router)
 app.include_router(blog.router)
 app.include_router(user.router)
