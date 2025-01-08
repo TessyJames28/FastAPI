6fdab0dc61b8e3d0100369927766011673cd8b35
Tessyjames <tessyjames28@gmail.com>
2025-01-08 23:54:23 +0100

    Implemented JWt token generation for login

--- blog/routers/authLogin.py
+++ blog/routers/authLogin.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm.session import Session
-from ..schemas import Login
+from datetime import timedelta
 from ..database import get_db
+from ..hashing import Hash
+from ..schemas import Login
+from ..token import create_access_token
 from .. import models
 
 
@@ -17,4 +20,14 @@ def login(request: Login, db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                             detail = f"Invalid credentials")
-    return user
+    
+    # Hashed password verification
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail = f"Incorrect password")
+    
+    # Generate JWT token and return it
+    # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.email}) #expires_delta=access_token_expires
+    
+    return {"access_token": access_token, "token_type": "bearer"}
