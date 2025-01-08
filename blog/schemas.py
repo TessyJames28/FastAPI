15cdf444568407f4082a82f12bd23d9bd4ffe8e8
Tessyjames <tessyjames28@gmail.com>
2025-01-08 23:52:34 +0100

    Added schema for JWT token

--- blog/schemas.py
+++ blog/schemas.py
@@ -48,3 +48,13 @@ class ShowBlog(BaseModel):
 class Login(BaseModel):
     username:str
     password:str
+
+
+# Schema for the token
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
