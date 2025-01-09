f6ce31508b4a9e354618ee686465105d96c2ff59
Tessyjames <tessyjames28@gmail.com>
2025-01-09 22:30:07 +0100

    Added Authorization for all blog APIs

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,7 +1,8 @@
 from ..database import get_db
 from fastapi import APIRouter, Depends, status
-from ..schemas import Blog, ShowBlog
+from ..schemas import Blog, ShowBlog, User
 from ..repository import blog
+from ..oauth2 import get_current_user
 from sqlalchemy.orm.session import Session
 from typing import List
 
@@ -13,34 +14,34 @@ router = APIRouter(
 
 
 @router.get('', response_model=List[ShowBlog])
-def get_all(db: Session = Depends(get_db)):
+def get_all(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     # Returns all blogs
     # The use of List from typing retrieve the blog titles and body as a list
     return blog.get_all(db)
 
 
 @router.post('', status_code=status.HTTP_201_CREATED)
-def create(request: Blog, db:Session = Depends(get_db)):
+def create(request: Blog, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     # Create blog based on the schema: using request body
     return blog.create(request, db)
 
 
 @router.delete('/{id}')
-def destroy(id: int, db:Session = Depends(get_db)):
+def destroy(id: int, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     # Delete an element on the db using the id
    return blog.destroy(id, db)
 
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: Blog, db:Session = Depends(get_db)):
+def update(id: int, request: Blog, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     # Update the blog based on id. request.dict() convert pydantic model into a dictionary
     return blog.update(id, request, db)
 
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=ShowBlog)
-def get_by_id(id: int, db: Session = Depends(get_db)):
+def get_by_id(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     # Retrieve blog based on given id
     # The response_model with the defined schema acts like the serializer on Django
     return blog.get_by_id(id, db)
